3fa12be391fd45da83ed90afccab64e551b5465a
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-06-10 11:48:31 +0200

    Add index.php for application entry point and routing setup

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ ."/app/helpers/bootstrap.php";
+require_once __DIR__ . '/app/core/Database.php';
+require_once __DIR__ . '/app/core/Response.php';
+require_once __DIR__ . '/app/models/Todo.php';
+require_once __DIR__ . '/app/controllers/TodoController.php';
+require_once __DIR__ . '/app/core/Router.php';
+
+header('Content-Type: application/json; charset=utf-8'); //JSON als Antwort
+header('Access-Control-Allow-Origin: *');//Erlaubt Anfragen von anderen Domains (CORS).
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');//Erlaubt Content-Type: application/json
+
+// Methode + URL einlesen
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = preg_replace(
+    '#^' . preg_quote(BASE_PATH, '#') . '/?#',
+    '',
+    $uri
+);
+
+$uri = trim($uri, '/');
+// Router startet
+$router = new Router($method, $uri);
+#var_dump($router);
+$router->dispatch();
+?>
