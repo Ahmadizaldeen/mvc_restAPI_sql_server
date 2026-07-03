43eff4e89a3a8d5efec549b60d3566e687c7ba32
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-07-04 00:45:14 +0200

    Refactor connectDB method to use try-catch for error handling and streamline connection logic

--- app/core/Database.php
+++ app/core/Database.php
@@ -2,21 +2,18 @@
 class Database
 {
     private static ?PDO $connection = null;
-
-    public static function connectDB(): PDO
-    {
-        if (self::$connection === null) {
-            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
-            self::$connection = new PDO(
-                $dsn,
-                DB_USER,
-                DB_PASS
-            );
-            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Exceptions bei Datenbankfehlern
-         	self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);// Fetch als assoziatives Array
-
-        }
-        return self::$connection;
-    }
+    public static function connectDB(): PDO 
+	{
+		try {
+			$dsn = "mysql:host=".DB_HOST. ";dbname=" . DB_NAME. ";charset=utf8mb4";
+			self::$connection = new PDO ($dsn, DB_USER, DB_PASS);
+			self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Exceptions bei Datenbankfehlern
+			self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);// Fetch als assoziatives Array
+			return self::$connection;
+		} catch (PDOException $e) {
+            Response::json(['error' => $e->getMessage()], 404);
+			die();
+		}
+	}
 }
 
