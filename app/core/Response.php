a723a1959b170eda9b8c586024b42bfece6ad9ee
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-07-04 00:48:41 +0200

    Refactor Response class to ensure JSON content type is set before response

--- app/core/Response.php
+++ app/core/Response.php
@@ -5,6 +5,7 @@ class Response
 {
     public static function json(mixed $data, int $status = 200): void
     {
+        header('Content-type: application/json; charset=utf-8');
         http_response_code($status);
         echo json_encode($data, JSON_UNESCAPED_UNICODE);//Verhindert die Umwandlung von Umlauten und Unicode-Zeichen
         exit; // Server beenden nach Json antwort
