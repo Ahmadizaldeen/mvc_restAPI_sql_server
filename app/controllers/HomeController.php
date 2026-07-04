36d33465f009a9283de5e4513699eabbd00ee3ee
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-07-04 04:23:46 +0200

    Remove debug echo statement from HomeController constructor

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 class HomeController{
     public function __construct(){
-        echo"Sex and love";
+        
     }
     public function index(){
         Response::json(["name" => "Todo API", "Version" => "2.1.1", "status" => "running"]);
