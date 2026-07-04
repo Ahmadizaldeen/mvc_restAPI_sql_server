f274963f9da399bd9c3c939878a87fb9f5c8deff
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-07-04 03:29:56 +0200

    Fix comment typo , Testability bug

--- app/core/Response.php
+++ app/core/Response.php
@@ -8,6 +8,6 @@ class Response
         header('Content-type: application/json; charset=utf-8');
         http_response_code($status);
         echo json_encode($data, JSON_UNESCAPED_UNICODE);//Verhindert die Umwandlung von Umlauten und Unicode-Zeichen
-        exit; // Server beenden nach Json antwort
+        exit; // Server beenden nach Json antwort!!!
     }
 }
